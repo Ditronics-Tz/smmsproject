e9f6ab512e7fe12aa92fd7de58ae3c8706f4cf58
godlisten-honest <your_email@example.com>
2025-02-26 19:35:02 +0300

    Another changes

--- smmsproject/settings.py
+++ smmsproject/settings.py
@@ -152,24 +152,24 @@ WSGI_APPLICATION = 'smmsproject.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
-
-# ----- DATABASE PRODUCTION -----
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'smmsdb',
-#         'USER': 'postgres',
-#         'PASSWORD': '123456789',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+# ----- DATABASE PRODUCTION -----
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smmsdb',
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
