9f09e44b592afaf3f4beba36d952a7331407de68
godlisten-honest <your-email@example.com>
2025-03-02 21:49:18 +0300

    update requirements

--- smmsproject/settings.py
+++ smmsproject/settings.py
@@ -149,7 +149,7 @@ WSGI_APPLICATION = 'smmsproject.wsgi.application'
 #         'USER': 'postgres',
 #         'PASSWORD': '1234',
 #         'HOST': '127.0.0.1',
-#         'PORT': '5433',
+#         'PORT': '5432',
 #     }
 # }
 
